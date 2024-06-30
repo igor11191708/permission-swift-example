92911f3685298e1cf221fe38e306ff8bd73dcb31
Igor <igorshelopaev@gmail.com>
2024-06-30 11:42:59 +0200

    Initial Commit

---
+++ permission-swift-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  permission-swift-example
+//
+//  Created by Igor  on 30.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ permission-swift-example/permission_swift_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  permission_swift_exampleApp.swift
+//  permission-swift-example
+//
+//  Created by Igor  on 30.06.24.
+//
+
+import SwiftUI
+
+@main
+struct permission_swift_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
